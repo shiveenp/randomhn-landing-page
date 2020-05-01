6ddd349eb44016805a0eb458366ea68749e28702
shiveenpandita <shiveenpandita@gmail.com>
2020-05-01 17:41:48 +1000

    feat(Link button): Changes the way link redrection works
    In accordance with the changes in the lambda, this change uses the item
    ID returned by the user to generate the redirection link using the
    `window.location` method. The advanatage here is that we cna show a
    little loading icon to the user while the redirection happens.

--- README.md
+++ README.md
@@ -1,6 +1,6 @@
 # randomhn-landing-page
 
-> My brilliant Nuxt.js project
+This is the parent repository for random hn landing page.
 
 ## Build Setup
 
@@ -18,5 +18,3 @@ $ npm run start
 # generate static project
 $ npm run generate
 ```
-
-For detailed explanation on how things work, check out [Nuxt.js docs](https://nuxtjs.org).
--- pages/index.vue
+++ pages/index.vue
@@ -7,27 +7,40 @@
             Random HN
           </h1>
           <p class="subtitle has-text-centered">
-            Click the button below and it will take you to a random HN story. HN is a great place to discover, read and take part in various topics and discussions. Unlike other platforms (like twitter, reddit etc.) HN provides a more nuanced and focussed discussion platform.
+            Click the button below and it will take you to a random hacker news story. HN is a great place to discover, read and
+            take part in various topics and discussions. Unlike other platforms (like twitter, reddit etc.) HN provides
+            a more nuanced and focused discussion platform.
           </p>
+          <div class="has-text-centered">
+            <b-button :loading="isLoading" @click="takeToNewHnLink" class="hn-theme">Jump In 🚀</b-button>
+          </div>
         </div>
       </div>
-    </section>
-    <section>
-      <div class="has-text-centered">
-        <b-button @click="takeToNewHnLink" class="hn-theme">Take Me Somewhere</b-button>
-      </div>
-    </section>
-    <section>
     </section>
   </div>
 </template>
 
 <script>
+  import axios from 'axios';
+
   export default {
     name: 'HomePage',
+    data() {
+      return {
+        isLoading: false
+      }
+    },
     methods: {
       takeToNewHnLink() {
-        window.location = 'https://qxjoqsucf5.execute-api.ap-southeast-2.amazonaws.com/default/randomhn';
+        this.isLoading = true;
+        axios.get('https://qxjoqsucf5.execute-api.ap-southeast-2.amazonaws.com/default/randomhn')
+          .then(response => {
+            const itemId = response.data;
+            window.location = `https://news.ycombinator.com/item?id=${itemId}`;
+          }).catch(error => {
+          console.log(error);
+          this.isLoading = false;
+        })
       }
     }
   }
